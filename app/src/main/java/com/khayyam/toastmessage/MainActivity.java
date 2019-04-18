e6deda6977434d3cc4adce68bd4342106eb5b97a
khayyam.abdinov <khayyam.abdinov@voidaint.com>
2019-04-18 23:14:08 +0400

    Testing Library Result:Working

--- app/src/main/java/com/khayyam/toastmessage/MainActivity.java
+++ app/src/main/java/com/khayyam/toastmessage/MainActivity.java
@@ -3,12 +3,14 @@ package com.khayyam.toastmessage;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.khayyam.toastlib.ToastMessage;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        ToastMessage.colorfulToast(MainActivity.this,"This is toast",R.color.white,R.color.colorPrimary,R.drawable.ic_android_green_24dp);
     }
 }
